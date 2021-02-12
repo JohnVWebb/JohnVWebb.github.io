50b74d6f24233b74ddb9fe24c129c1ab9321d201
JohnVWebb <jvw1225@gmail.com>
2021-02-12 16:28:23 -0700

    all requirements except automatic mining

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let cheese = 0
+let mineModifier = 0
 
 let clickUpgrades = {
     pickaxe: {
@@ -9,7 +10,7 @@ let clickUpgrades = {
     miningLaser: {
         price: 25,
         quantity: 0,
-        multiplier: 10
+        multiplier: 5
     }
 }
 
@@ -17,12 +18,12 @@ let automaticUpgrades = {
     rover: {
         price: 50,
         quantity: 0,
-        multiplier: 20
+        multiplier: 10
     },
     lunarBase: {
         price: 100,
         quantity: 0,
-        multiplier: 50
+        multiplier: 25
     }
 }
 
@@ -35,8 +36,8 @@ function buyPickaxe(){
     if( cheese >= clickUpgrades.pickaxe.price){
     clickUpgrades.pickaxe.quantity++
     cheese -= clickUpgrades.pickaxe.price
-    console.log(clickUpgrades.pickaxe.quantity)
-    console.log(cheese)
+    clickUpgrades.pickaxe.price += 5
+    mineModifier += clickUpgrades.pickaxe.multiplier
     }
     update()
 }
@@ -45,8 +46,8 @@ function buyMiningLaser(){
     if( cheese >= clickUpgrades.miningLaser.price){
     clickUpgrades.miningLaser.quantity++
     cheese -= clickUpgrades.miningLaser.price
-    console.log(clickUpgrades.miningLaser.quantity)
-    console.log(cheese)
+    clickUpgrades.miningLaser.price += 10
+    mineModifier += clickUpgrades.miningLaser.multiplier
     }
     update()
 }
@@ -55,8 +56,8 @@ function buyRover(){
     if( cheese >= automaticUpgrades.rover.price){
     automaticUpgrades.rover.quantity++
     cheese -= automaticUpgrades.rover.price
-    console.log(automaticUpgrades.rover.quantity)
-    console.log(cheese)
+    automaticUpgrades.rover.price += 25
+    mineModifier += automaticUpgrades.rover.multiplier
     }
     update()
 }
@@ -65,8 +66,8 @@ function buyLunarBase(){
     if( cheese >= automaticUpgrades.lunarBase.price){
     automaticUpgrades.lunarBase.quantity++
     cheese -= automaticUpgrades.lunarBase.price
-    console.log(automaticUpgrades.lunarBase.quantity)
-    console.log(cheese)
+    automaticUpgrades.lunarBase.price += 50
+    mineModifier += automaticUpgrades.lunarBase.multiplier
     }
     update()
 }
@@ -77,6 +78,18 @@ function update(){
     }
     document.getElementById('playerCheeseAmount').innerText = cheese.toString()
     document.getElementById('pickaxePrice').innerText = ' ' + clickUpgrades.pickaxe.price.toString() 
+    document.getElementById('miningLaserPrice').innerText = ' ' + clickUpgrades.miningLaser.price.toString()
+    document.getElementById('roverPrice').innerText = ' ' + automaticUpgrades.rover.price.toString()
+    document.getElementById('lunarBasePrice').innerText = ' ' + automaticUpgrades.lunarBase.price.toString()
+    document.getElementById('pickaxeAmount').innerText = ' ' + clickUpgrades.pickaxe.quantity.toString() 
+    document.getElementById('miningLaserAmount').innerText = ' ' + clickUpgrades.miningLaser.quantity.toString()
+    document.getElementById('roverAmount').innerText = ' ' + automaticUpgrades.rover.quantity.toString()
+    document.getElementById('lunarBaseAmount').innerText = ' ' + automaticUpgrades.lunarBase.quantity.toString()
+    document.getElementById('pickaxeMultiplier').innerText = ' ' + clickUpgrades.pickaxe.multiplier.toString()
+    document.getElementById('miningLaserMultiplier').innerText = ' ' + clickUpgrades.miningLaser.multiplier.toString()
+    document.getElementById('roverMultiplier').innerText = ' ' + automaticUpgrades.rover.multiplier.toString()
+    document.getElementById('lunarBaseMultiplier').innerText = ' ' + automaticUpgrades.lunarBase.multiplier.toString()
+    document.getElementById('playerMultiplier').innerText = ' ' + mineModifier
 }
 
 update()
